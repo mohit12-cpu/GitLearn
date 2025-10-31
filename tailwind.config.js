a9214a6c74196b14f963235c8cb9885fe61eaef6
Mohit Shrestha <mohitstha29@gmail.com>
2025-10-31 20:34:55 +0545

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+// Tailwind config
+tailwind.config = {
+    darkMode: 'class',
+    theme: {
+        extend: {
+            fontFamily: {
+                inter: ['Inter', 'sans-serif'],
+            },
+            colors: {
+                'primary': '#4f46e5',
+                'secondary': '#ec4899',
+                'dark-bg': '#111827',
+                'dark-card': '#1f2937',
+                'dark-text': '#d1d5db',
+            }
+        }
+    }
+}
